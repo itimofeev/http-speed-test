c99433cb4472056100df751dd1340da15c8f0cca
ilya.timofeev <ilya.timofeev@ru.axxonsoft.com>
2019-03-05 08:41:51 +0300

    client

---
+++ client.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/itimofeev/http-speed-test/humanize"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+type countingWriter struct {
+	writeBytes uint64
+}
+
+func (r *countingWriter) Write(p []byte) (int, error) {
+	r.writeBytes += uint64(len(p))
+	return len(p), nil
+}
+
+func runClient() {
+	serverAddress := "localhost:13579"
+	size := 100 * 1024 * 1024
+	start := time.Now()
+
+	resp, err := http.Get(fmt.Sprintf("http://%s/download?size=%d", serverAddress, size))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writer := &countingWriter{}
+	_, err = io.Copy(writer, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	duration := time.Now().Sub(start)
+	log.Printf("downloaded %s for %s, speed %s", humanize.IBytes(writer.writeBytes), duration, formatSpeed(writer.writeBytes, duration))
+}
+
+func formatSpeed(bytes uint64, duration time.Duration) string {
+	speed := float64(bytes) / duration.Seconds()
+	if speed < 1024 {
+		return fmt.Sprintf("%f b/s", speed)
+	}
+	if speed < 1024*1024 {
+		return fmt.Sprintf("%f Kib/s", speed/1024)
+	}
+	if speed < 1024*1024*1024 {
+		return fmt.Sprintf("%f Mib/s", speed/1024/1024)
+	}
+	return fmt.Sprintf("%f Gib/s", speed/1024/1024/1024)
+}
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "flag"
+
 func main() {
+	isServerPtr := flag.String("mode", "server", "can be server or client (default is server)")
+
+	flag.Parse()
+
+	if *isServerPtr == "client" {
+		runClient()
+		return
+	}
 	runServer()
 }
